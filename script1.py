6305a319b76d4e689367f77d7098385cd5c05399
DennisDam439 <dend@flemingcollege.ca>
2024-05-27 13:56:50 -0400

    Added comment to script1.py

---
+++ script1.py
@@ -0,0 +1,6 @@
+Now is better than never.
+Although never is often better than *right* now.
+
+# Print lines fromZen of Python
+print("There sghould be one-- and preferably only one --obvious way to do it.")
+print("Although that way may be not obvious at first unless you're Dutch.")
