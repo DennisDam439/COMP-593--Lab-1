e603eec73c293f48b4eea22ecb593c54e5eb4360
DennisDam439 <dend@flemingcollege.ca>
2024-05-27 14:22:50 -0400

    Revert "modified both script files"
    This reverts commit e9e61e7f5d2877f0c39ff94986715cfb15b8af13.

--- script1.py
+++ script1.py
@@ -1,4 +1,5 @@
 Now is better than never.
 Although never is often better than *right* now.
 
-# Print lines from Zen of Python
+# Print lines fromZen of Python
+print("There sghould be one-- and preferably only one --obvious way to do it.")
