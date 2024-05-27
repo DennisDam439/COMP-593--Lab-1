bf1cb592d53d06122cb4df559118fc25a4f6f320
DennisDam439 <dend@flemingcollege.ca>
2024-05-27 14:07:06 -0400

    comment added to script1.py
    deleted line a last line of print from script1.py

---
+++ script2.py
@@ -0,0 +1,6 @@
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+
+# Print lines fromZen of Python
+Print("There sghould be one-- and preferably only one --obvious way to do it.")
+Print("Although that way may be not obvious at first unless you're Dutch.")
