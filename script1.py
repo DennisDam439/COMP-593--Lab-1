e9e61e7f5d2877f0c39ff94986715cfb15b8af13
DennisDam439 <dend@flemingcollege.ca>
2024-05-27 14:22:07 -0400

    modified both script files
    -deleted script command from script1.py
    -addded comment to script2.py

--- script1.py
+++ script1.py
@@ -1,5 +1,4 @@
 Now is better than never.
 Although never is often better than *right* now.
 
-# Print lines fromZen of Python
-print("There sghould be one-- and preferably only one --obvious way to do it.")
+# Print lines from Zen of Python
--- script2.py
+++ script2.py
@@ -2,5 +2,5 @@ Unless explicitly silenced.
 In the face of ambiguity, refuse the temptation to guess.
 
 # Print lines fromZen of Python
-Print("There sghould be one-- and preferably only one --obvious way to do it.")
-Print("Although that way may be not obvious at first unless you're Dutch.")
+print("There sghould be one-- and preferably only one --obvious way to do it.")
+print("Although that way may be not obvious at first unless you're Dutch.")
