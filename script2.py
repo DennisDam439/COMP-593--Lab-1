cda668388b1d424e1711d7f680864a01cf6e23f1
DennisDam439 <dend@flemingcollege.ca>
2024-05-27 14:37:30 -0400

    Put comment back in script2.py
    Comment was unintentionally deleted by revert.

--- script2.py
+++ script2.py
@@ -2,5 +2,6 @@ Unless explicitly silenced.
 In the face of ambiguity, refuse the temptation to guess.
 
 # Print lines fromZen of Python
-Print("There sghould be one-- and preferably only one --obvious way to do it.")
-Print("Although that way may be not obvious at first unless you're Dutch.")
+# This is a good programming advice 
+print("There sghould be one-- and preferably only one --obvious way to do it.")
+print("Although that way may be not obvious at first unless you're Dutch.")
